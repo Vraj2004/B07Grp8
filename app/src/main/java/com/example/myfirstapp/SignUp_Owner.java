7900b9cceff312f6ad9fa11f78cd576420603035
Krit Grover <kritgrover@gmail.com>
2023-08-01 01:01:23 -0400

    Auth and DB connected for Owner

--- app/src/main/java/com/example/myfirstapp/SignUp_Owner.java
+++ app/src/main/java/com/example/myfirstapp/SignUp_Owner.java
@@ -17,6 +17,8 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
@@ -27,6 +29,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class SignUp_Owner extends AppCompatActivity {
 
@@ -43,56 +47,47 @@ public class SignUp_Owner extends AppCompatActivity {
         final EditText editStoreName = findViewById(R.id.Store_Name);
         final EditText editPasswordConfirm = findViewById(R.id.confirm_password_owner);
         final Button btn = findViewById(R.id.sign_up_btn_owner);
+        final FirebaseAuth fAuth = FirebaseAuth.getInstance();
 
 
         btn.setOnClickListener(view -> {
                 String email, password, username, storeName, passwordConfirm;
-                email = String.valueOf(editEmail.getText().toString());
-                password = String.valueOf(editPassword.getText().toString());
-                passwordConfirm = String.valueOf(editPasswordConfirm.getText().toString());
-                username = String.valueOf(editUser.getText().toString());
-                storeName = String.valueOf(editStoreName.getText().toString());
+                email = editEmail.getText().toString();
+                password = editPassword.getText().toString();
+                passwordConfirm = editPasswordConfirm.getText().toString();
+                username = editUser.getText().toString();
+                storeName = editStoreName.getText().toString();
 
                 if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(username)
                 || TextUtils.isEmpty(storeName) || TextUtils.isEmpty(passwordConfirm)) {
                     Toast.makeText(SignUp_Owner.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
                 }
-                else if (!password.equals(passwordConfirm)){
+
+                if (!password.equals(passwordConfirm)){
                     Toast.makeText(SignUp_Owner.this, "Password does not match", Toast.LENGTH_SHORT).show();
                 }
-                else
-                {
-                    dbRef.child("Sellers").addListenerForSingleValueEvent(new ValueEventListener(){
 
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            if (snapshot.hasChild(email))
-                            {
-                                Toast.makeText(SignUp_Owner.this, "This email is already registered",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                            else
-                            {
-                                dbRef.child("Sellers").child(email).child("username").setValue(username);
-                                dbRef.child("Sellers").child(email).child("password").setValue(password);
-                                dbRef.child("Sellers").child(email).child("store name").setValue(storeName);
-                                dbRef.child("Sellers").child(email).child("personality").setValue("Seller");
-                                Toast.makeText(SignUp_Owner.this, "Account created successfully!",
-                                        Toast.LENGTH_SHORT).show();
-
-                                Intent seller_intent = new Intent(getApplicationContext(), Login.class);
-                                startActivity(seller_intent);
-                                finish();
-                            }
-
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
+            fAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(SignUp_Owner.this, "Account Created", Toast.LENGTH_SHORT).show();
+                    HashMap<String, Object> add = new HashMap<>();
+                    add.put("Username", username);
+                    add.put("Store Name", storeName);
+                    add.put("Email", email);
+                    add.put("isOwner", 1);
+                    dbRef.child("Users").push().setValue(add);
+                    startActivity(new Intent(getApplicationContext(), Login.class));
+                    finish();
                 }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(SignUp_Owner.this, "Failed to Create Account!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), SignUp_Customer.class));
+                    finish();
+                }
+            });
         });
     }
 }
