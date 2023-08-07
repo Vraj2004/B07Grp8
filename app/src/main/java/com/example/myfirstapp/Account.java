e7ad0da22f7ce8196e2cb563da9652b303432b7f
pranavm04 <pranav.mundhra@mail.utoronto.ca>
2023-08-07 15:45:50 -0400

    Added frontend and sign out functionality for account page

--- app/src/main/java/com/example/myfirstapp/Account.java
+++ app/src/main/java/com/example/myfirstapp/Account.java
@@ -1,7 +1,73 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Account extends AppCompatActivity {
 
+    ImageButton home;
+    ImageButton account;
+    ImageButton orders;
+
+    private Button signOut;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        home = findViewById(R.id.home_button);
+        orders = findViewById(R.id.orders_button);
+        account = findViewById(R.id.account_button);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), home_seller.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        orders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(),item_cart.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(), Account.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+        signOut = findViewById(R.id.signout_button);
+        String productName = getIntent().getStringExtra("STORE_NAME");
+        Toast.makeText(this, productName, Toast.LENGTH_SHORT).show();
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
+
+    }
 }
