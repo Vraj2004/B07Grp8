78d69a1c043a52835759fd05cd93e6898e871cd4
pranavm04 <pranav.mundhra@mail.utoronto.ca>
2023-08-07 13:38:11 -0400

    Added toolbar to checkout page and commented out the unused cart button

--- app/src/main/java/com/example/myfirstapp/checkoutPage.java
+++ app/src/main/java/com/example/myfirstapp/checkoutPage.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import org.w3c.dom.Text;
 
@@ -17,14 +19,47 @@ public class checkoutPage extends AppCompatActivity {
     Button order;
     EditText firstName, lastName, emailAddress, Address;
     EditText postalCode, creditCardNumber, csv, expiryDate;
-
-
+    ImageButton home;
+    ImageButton account;
+    ImageButton orders;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_page);
+
+        home = findViewById(R.id.home_button);
+        orders = findViewById(R.id.orders_button);
+        account = findViewById(R.id.account_button);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), home_seller.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        orders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(),item_cart.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(),Account.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
         order = findViewById(R.id.finalOrderButton);
 
