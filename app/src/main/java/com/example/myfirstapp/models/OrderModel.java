717c084de2725faef6f06eeb6bd218ead1f52d0d
kritgrover <kritgrover@gmail.com>
2023-08-07 15:00:51 -0400

    new model

---
+++ app/src/main/java/com/example/myfirstapp/models/OrderModel.java
@@ -0,0 +1,50 @@
+package com.example.myfirstapp.models;
+
+public class OrderModel {
+    String orderID;
+    String status;
+    String userID;
+    String products;
+
+    public OrderModel() {
+    }
+
+    public OrderModel(String orderID, String status, String userID, String products) {
+        this.orderID = orderID;
+        this.status = status;
+        this.userID = userID;
+        this.products = products;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getProducts() {
+        return products;
+    }
+
+    public void setProducts(String products) {
+        this.products = products;
+    }
+}
