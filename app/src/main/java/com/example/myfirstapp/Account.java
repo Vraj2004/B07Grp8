d9ad0b5b2557e51c89ea105081b8bbfe914328dc
Krit Grover <kritgrover@gmail.com>
2023-08-08 11:10:21 -0400

    show user data on account page

--- app/src/main/java/com/example/myfirstapp/Account.java
+++ app/src/main/java/com/example/myfirstapp/Account.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Account extends AppCompatActivity {
 
@@ -19,6 +26,9 @@ public class Account extends AppCompatActivity {
 
     private Button signOut;
     FirebaseAuth mAuth;
+    DatabaseReference dbRef;
+    TextView showUser, showPass, showEmail;
+    String email, password, username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +37,14 @@ public class Account extends AppCompatActivity {
         home = findViewById(R.id.home_button);
         orders = findViewById(R.id.orders_button);
         account = findViewById(R.id.account_button);
+
+        showEmail = findViewById(R.id.email_content);
+        showUser = findViewById(R.id.username_content);
+        showPass = findViewById(R.id.password_content);
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        showContent();
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,7 +74,6 @@ public class Account extends AppCompatActivity {
             }
         });
 
-        mAuth = FirebaseAuth.getInstance();
         signOut = findViewById(R.id.signout_button);
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,5 +84,25 @@ public class Account extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void showContent() {
+
+        dbRef.child("Users").child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                password = snapshot.child("password").getValue(String.class);
+                email = snapshot.child("email").getValue(String.class);
+                username = snapshot.child("username").getValue(String.class);
+                showEmail.setText(email);
+                showPass.setText(password);
+                showUser.setText(username);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myfirstapp/AccountShopper.java
+++ app/src/main/java/com/example/myfirstapp/AccountShopper.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountShopper extends AppCompatActivity {
 
@@ -20,6 +27,9 @@ public class AccountShopper extends AppCompatActivity {
 
     private Button signOut;
     FirebaseAuth mAuth;
+    DatabaseReference dbRef;
+    TextView showUser, showPass, showEmail;
+    String email, password, username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +39,14 @@ public class AccountShopper extends AppCompatActivity {
         orders = findViewById(R.id.orders_button);
         account = findViewById(R.id.account_button);
         cart = findViewById(R.id.cart_button);
+
+        showEmail = findViewById(R.id.email_content);
+        showUser = findViewById(R.id.username_content);
+        showPass = findViewById(R.id.password_content);
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        showContent();
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +86,7 @@ public class AccountShopper extends AppCompatActivity {
             }
         });
 
-        mAuth = FirebaseAuth.getInstance();
+
         signOut = findViewById(R.id.signout_button);
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,5 +97,25 @@ public class AccountShopper extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void showContent() {
+
+        dbRef.child("Users").child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                password = snapshot.child("password").getValue(String.class);
+                email = snapshot.child("email").getValue(String.class);
+                username = snapshot.child("username").getValue(String.class);
+                showEmail.setText(email);
+                showPass.setText(password);
+                showUser.setText(username);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
